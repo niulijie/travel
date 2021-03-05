c7eaa9070a6d0d6824b03f6638e261856ef46f27
niulijie <niulijie123>
2021-03-05 18:09:29 +0800

    测试spring容器

--- springbootTest/src/main/java/com/niulijie/springboot/SpringBootTestApplication.java
+++ springbootTest/src/main/java/com/niulijie/springboot/SpringBootTestApplication.java
@@ -2,8 +2,12 @@ package com.niulijie.springboot;
 
 //@EnableAutoConfiguration//启用springboot自动配置
 
+import com.niulijie.springboot.test.MainScanConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 1.定义扫描的路径从中找出标识了需要装配的类自动装配到spring的bean容器中,类似于<context:component-scan base-package="">
@@ -14,6 +18,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootTestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class,args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SpringBootTestApplication.class, args);
+        String[] beanDefinitionNames = configurableApplicationContext.getBeanDefinitionNames();
+        for (String beanName: beanDefinitionNames) {
+            System.out.println(beanName);
+        }
+        MainScanConfig bean = configurableApplicationContext.getBean(MainScanConfig.class);
     }
 }
---
+++ springbootTest/src/main/java/com/niulijie/springboot/test/MainScanConfig.java
@@ -0,0 +1,14 @@
+package com.niulijie.springboot.test;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 主配置类  包扫描
+ * @author edz
+ */
+@ComponentScan(value="com.niulijie.springboot")
+@Configuration
+public class MainScanConfig {
+}
