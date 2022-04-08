c135430429459932ddaeb6968bd01d784d0607f1
niulijie <niulijie123>
2022-04-08 17:09:20 +0800

    硅谷-Map和Model类型参数放入request中原理

--- springbootweb/src/main/java/com/niuljie/springboot/controller/HelloController.java
+++ springbootweb/src/main/java/com/niuljie/springboot/controller/HelloController.java
@@ -17,8 +17,31 @@ public class HelloController {
      *          --> processRequest (org.springframework.web.servlet.FrameworkServlet#processRequest)
      *              --> doService (org.springframework.web.servlet.DispatcherServlet#doService)
      *                  --> doDispatch (org.springframework.web.servlet.DispatcherServlet#doDispatch)
-     *                      --> getHandler (org.springframework.web.servlet.DispatcherServlet#getHandler) 找到当前请求使用哪个Handler(Controller的方法)处理
-     *                          --> RequestMappingHandlerMapping：保存了所有@RequestMapping 和handler的映射规则。
+     *                      #--> getHandler (org.springframework.web.servlet.DispatcherServlet#getHandler) 找到当前请求使用哪个Handler(Controller的方法)处理
+     *                          --> RequestMappingHandlerMapping：保存了所有@RequestMapping 和handler的映射规则。(5种)
+     *                              --> RequestMappingHandlerAdapter 处理器适配器(4种)
+     *                                  --> 执行目标方法 HandlerAdapter#handle(HttpServletRequest, HttpServletResponse, Handler)
+     *                                      进入RequestMappingHandlerAdapter#handleInternal,执行目标方法--RequestMappingHandlerAdapter#invokeHandlerMethod,返回ModelAndView
+     *                                      ① HandlerMethodArgumentResolver参数解析器，确定将要执行的目标方法的每一个参数的值是什么--26个;
+     *                                        判断当前解析器是否支持解析这种参数，支持就调用 resolveArgument
+     *                                      ② HandlerMethodReturnValueHandler返回值处理器--15个
+     *                                      ③ 将所有26个参数解析器和15个返回值处理器都封装到invocableMethod方法中，ServletInvocableHandlerMethod#invokeAndHandle --> invokeForRequest真正执行目标方法
+     *                                          Ⅰ.InvocableHandlerMethod#getMethodArgumentValues 确定目标方法每一个参数得值
+     *                                          Ⅱ.HandlerMethodArgumentResolverComposite#resolveArgument
+     *                                              HandlerMethodArgumentResolverComposite#getArgumentResolver遍历26个参数解析器，判断哪个支持解析这个参数，并且放入缓存
+     *                                              HandlerMethodArgumentResolver#resolveArgument 从requestAttribute中拿出参数值
+     *                       #--> org.springframework.web.servlet.DispatcherServlet#processDispatchResult 处理派发结果
+     *                          --> org.springframework.web.servlet.DispatcherServlet#render 页面渲染
+     *                              --> view = resolveViewName(viewName, mv.getModelInternal(), locale, request) -- DispatcherServlet#resolveViewName
+     *                                  viewResolver.resolveViewName(viewName, locale)遍历4种视图解析器根据视图名进行视图解析
+     *                              --> view.render(mv.getModelInternal(), request, response) -- View#render 渲染页面
+     *                                  ① AbstractView#createMergedOutputModel(model, request, response) 将model值放进LinkedHashMap中
+     *                                  ② prepareResponse(request, response); 准备响应
+     *                                  ③ InternalResourceView#renderMergedOutputModel(mergedModel, getRequestToExpose(request), response); 渲染准备输入的的model值
+     *                                      Ⅰ.exposeModelAsRequestAttributes(model, request); 暴露model数据作为请求域属性 -- 遍历直接放入request中
+     *                                      Ⅱ.exposeHelpers(request);
+     *                                      Ⅲ.String dispatcherPath = prepareForRendering(request, response);
+     *                                      Ⅳ.RequestDispatcher rd = getRequestDispatcher(request, dispatcherPath);
      * 4.SpringBoot自动配置欢迎页的 WelcomePageHandlerMapping --> 访问 /能访问到index.htm;
      *   SpringBoot自动配置了默认的 RequestMappingHandlerMapping
      * @return
--- springbootweb/src/main/java/com/niuljie/springboot/controller/RequestController.java
+++ springbootweb/src/main/java/com/niuljie/springboot/controller/RequestController.java
@@ -1,19 +1,50 @@
 package com.niuljie.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class RequestController {
 
+    /**
+     * Servlet API：ServletRequestMethodArgumentResolver 处理以下的部分参数
+     * WebRequest、ServletRequest、MultipartRequest、 HttpSession、javax.servlet.http.PushBuilder、Principal、InputStream、Reader、HttpMethod、Locale、TimeZone、ZoneId
+     * @param request
+     * @return
+     */
     @GetMapping("/goto")
     public String goToPage(HttpServletRequest request){
+        request.setAttribute("msg", "成功了。。。。");
+        request.setAttribute("code", 200);
+        //转发到/success请求
+        return "forward:/success";
+    }
+
+    /**
+     * Map、Model类型的参数，会返回 mavContainer.getModel() -- ModelAndViewContainer#getModel() --> BindingAwareModelMap 是Model 也是Map
+     * @param map
+     * @param model
+     * @param request
+     * @param response
+     * @return
+     */
+    @GetMapping("/params")
+    public String testParam(Map<String, Object> map, Model model, HttpServletRequest request, HttpServletResponse response){
+        map.put("hello", "world666");
+        model.addAttribute("world", "hello555");
+
+        Cookie cookie = new Cookie("c1","v1");
+        cookie.setDomain("localhost");
+        response.addCookie(cookie);
         request.setAttribute("msg", "成功了。。。。");
         request.setAttribute("code", 200);
         //转发到/success请求
@@ -26,14 +57,17 @@ public class RequestController {
      */
     @ResponseBody
     @GetMapping("/success")
-    public Map<String,Object> success(@RequestAttribute("msg") String msg,
-                          @RequestAttribute("code") Integer code,
+    public Map<String,Object> success(@RequestAttribute(value = "msg", required = false) String msg,
+                          @RequestAttribute(value = "code", required = false) Integer code,
                           HttpServletRequest request){
         Map<String,Object> map = new HashMap<>();
         Object msg1 = request.getAttribute("msg");
+        Object hello = request.getAttribute("hello");
+        Object world = request.getAttribute("world");
         map.put("msg1",msg1);
         map.put("annotation_msg", msg);
-
+        map.put("hello", hello);
+        map.put("world", world);
         return map;
     }
 }
